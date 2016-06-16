7e803b86b6169fc3edf6c3308b7cde2083ea7eb6
yuval1982 <yuval1982@gmail.com>
2016-06-16 11:56:21 +0300

    added renderChals and getCurrChal, renamed class challenge to chal

--- js/chal.js
+++ js/chal.js
@@ -3,21 +3,25 @@ var gChals = [
     {
         id: 'chal1',
         name: 'What\'s in the picture?',
+        imgSrc: 'img/monster/2.png',
         isSolved: false
     },
     {
         id: 'chal2',
         name: 'Up to 100',
+        imgSrc: 'img/monster/2.png',
         isSolved: false
     },
     {
         id: 'chal3',
         name: 'The puzzles master',
+        imgSrc: 'img/monster/2.png',
         isSolved: false
     },
     {
         id: 'chal4',
         name: 'Fun counting',
+        imgSrc: 'img/monster/2.png',
         isSolved: false
     }
 ];
@@ -27,21 +31,37 @@ function getChals() {
     var state = JSON.parse(localStorage.getItem('gState'));
     if (!state) chals = gChals;
     else chals = state.chals;
-    
+
     return chals;
 }
 
 function getChalById(chalId) {}
+
 function getCurrChal() {
-    var gState = localStorage.getItem('gState');
-    if (gState) {
-        var currChal = JSON.parse(gState).currChal;
-    }
+    var state = JSON.parse(localStorage.getItem('gState'));
+    var currChal;
+    if (!state) currChal = gChals[0];
+    else currChal = state.chals.filter(function (chal) {
+        return (!chal.isSolved)
+    })[0];
 
     return currChal;
 }
 
-function renderChals() {}
+function renderChals() {
+    var chals = getChals();
+    var currChal = getCurrChal();
+    var strHTML = '';
+    chals.forEach(function (chal, i) {
+        strHTML += '<div class="col-md-3 col-xs-6 chal'+ ((chal.id <= currChal.id)? '':' locked') +'">';
+        strHTML += '<a href="'+ chal.id +'.html">';
+        strHTML += '<img src="'+ chal.imgSrc +'" class="img-responsive" alt="">';
+        strHTML += '</a></div>';
+    })
+    
+    var elChals = document.querySelector('.chals');
+    elChals.innerHTML = strHTML;
+}
 
 function reportSolved(chalId){
     var state = JSON.parse(localStorage.getItem('gState'));
