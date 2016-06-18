9d3f194434cdc4e1ccffb2a9f624d98e9224ecfe
yuval1982 <yuval1982@gmail.com>
2016-06-18 22:45:00 +0300

    fixed more merge-bugs in chal2.js, leaving chal3.html to rana

--- js/chal2.js
+++ js/chal2.js
@@ -4,11 +4,8 @@ var gBoard;
 var gLevel = 1;
 var chartType;
 function initGame() {
-<<<<<<< HEAD
-=======
-    
->>>>>>> 642d9dde429f5761a209adb5dd9f60e6b5698cbc
-    gBoard = buidBoard(chartType);
+
+    gBoard = buildBoard(chartType);
     document.querySelector('.board').style.display = 'block';
     startGame(gBoard);
 }
@@ -16,15 +13,11 @@ function getChartType(type) {
     chartType = type;   
     document.querySelector('.chartType').style.display = 'none';   
     initGame();
-<<<<<<< HEAD
-=======
     drop();
->>>>>>> 642d9dde429f5761a209adb5dd9f60e6b5698cbc
 }
-function buidBoard(chartType) {
+function buildBoard(chartType) {
     var arrayBoard = [];
     var start = chartType;
-<<<<<<< HEAD
     
     for(var i = start ;i <= 9   ;i++){
         arrayBoard[i] = [];
@@ -35,16 +28,7 @@ function buidBoard(chartType) {
     console.table(arrayBoard);
     return arrayBoard;
 }
-=======
-    for(var i = 0 ;i < 10   ;i++){
-        arrayBoard[i] = [];
-        start = chartType;
-        for(var j = 0 ;j < 10 ;j++ , start++ ){
-            arrayBoard[i][j] = i*10 + start;
-        }
-    }
-    return arrayBoard;
-}
+    
  function drop() {
         $("[data-row='1'][data-colm='3']" ).droppable({
         
@@ -57,16 +41,12 @@ function buidBoard(chartType) {
         $( "#answer td" ).draggable();
   }
     
->>>>>>> 642d9dde429f5761a209adb5dd9f60e6b5698cbc
 function startGame(board) {
     var gameBoard = board;
     for(var i = 0 ;i < gameBoard.length ;i++){
         renderRow(i, gameBoard);
-<<<<<<< HEAD
         randCell(i);
-=======
         playRow(i);
->>>>>>> 642d9dde429f5761a209adb5dd9f60e6b5698cbc
     }
 }
 function renderRow(row , board) {
@@ -79,25 +59,20 @@ function renderRow(row , board) {
         elTd.setAttribute('class', className);
         elTd.setAttribute('data-row', row);
         elTd.setAttribute('data-colm', i);
-<<<<<<< HEAD
         elTd.setAttribute('onclick', 'f(this)');
        
         
-=======
         // elTd.setAttribute('onclick', 'f(this)');
->>>>>>> 642d9dde429f5761a209adb5dd9f60e6b5698cbc
         elRow.appendChild(elTd);
     }
     document.querySelector('#tableGame').appendChild(elRow);
 }
-<<<<<<< HEAD
 function randCell(row) {
     var rand1 = Math.floor(Math.random()*10);
     var rand2 = Math.floor(Math.random()*10);
     
 }
-  
-=======
+
 function playRow(row) {
     var arryMissNum = [];
     arryMissNum.push(randCell(row)); 
@@ -130,6 +105,4 @@ function userTurn() {
         flag = true;
     }, 10000)
     return flag;
-} 
->>>>>>> 642d9dde429f5761a209adb5dd9f60e6b5698cbc
-
+}
